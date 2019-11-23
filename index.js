1c975d30ac2f7262422ce2763db10de81be86cfe
Aralek <nolwenn.myran@free.fr>
2019-11-23 17:36:46 +0100

    added empty routes and models files

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const DB_USERNAME = process.env.DB_USERNAME
-const DB_PASSWORD = process.env.DB_PASSWORD
-
 var express = require('express');
 var mongoose = require('mongoose'); 
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const DB_URL = process.env.DB_URL
+const PORT = parseInt(process.env.PORT)
+
+
+var app = express(); 
+
+const armorRoute = require('./routes/armor')
+const armRoute = require('./routes/arm')
+const helmetRoute = require('./routes/helmet')
+const legRoute = require('./routes/leg')
+const cloakRoute = require('./routes/cloak')
+const torsoRoute = require('./routes/torso')
+
+app.use('/', armorRoute)
+app.use('/arm', armRoute)
+app.use('/leg', legRoute)
+app.use('/helmet', helmetRoute)
+app.use('/cloak', cloakRoute)
+app.use('/torso', torsoRoute)
+
+mongoose.connect(DB_URL, { useNewUrlParser: true });
+
+app.listen(PORT)
