21ba2e574a6dfa9a7c8921c20c6c487559c74b5b
Oznerologos <lorenzo.logos@ynov.com>
2020-01-19 22:36:49 +0100

    armor list

--- frontend/src/app/armor/armor.component.ts
+++ frontend/src/app/armor/armor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-armor",
@@ -6,7 +7,15 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./armor.component.css"]
 })
 export class ArmorComponent implements OnInit {
-  constructor() {}
+  armors = [];
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.http.get("http://localhost:3000/armor").subscribe(value => {
+      for (let i = 0; i < Object.keys(value).length; i++) {
+        this.armors.push([value[i].name, value[i]._id, value[i].composition]);
+      }
+      console.log(this.armors);
+    });
+  }
 }
