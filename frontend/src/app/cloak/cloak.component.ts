795220d8c94bd6c4f67894f3c7a0840c910a32fa
Oznerologos <lorenzo.logos@ynov.com>
2020-01-19 19:14:32 +0100

    cloak with new update

--- frontend/src/app/cloak/cloak.component.ts
+++ frontend/src/app/cloak/cloak.component.ts
@@ -17,6 +17,11 @@ export class CloakComponent implements OnInit {
     cloakValue: new FormControl("")
   });
 
+  // FORMGROUP - MODIFICATION
+  modificationCloakForm = new FormGroup({
+    cloakName: new FormControl(""),
+    cloakValue: new FormControl("")
+  });
   cloaks = [];
   submitted = false;
 
@@ -24,7 +29,6 @@ export class CloakComponent implements OnInit {
 
   ngOnInit() {
     this.http.get("http://localhost:3000/cloak").subscribe(value => {
-      // tslint:disable-next-line: prefer-for-of
       for (let i = 0; i < Object.keys(value).length; i++) {
         this.cloaks.push([value[i].name, value[i].value, value[i]._id]);
       }
@@ -33,34 +37,17 @@ export class CloakComponent implements OnInit {
   }
 
   onSubmit() {
-    const requestType = (document.getElementById("type") as HTMLInputElement)
-      .value;
+    this.submitted = true;
+    const jsonCloak = JSON.parse(
+      '{"name":"' +
+        this.createCloakForm.value["cloakName"] +
+        '", "value": ' +
+        this.createCloakForm.value["cloakValue"] +
+        "}"
+    );
 
-    if (requestType === "add") {
-      this.submitted = true;
-      const jsonCloak = JSON.parse(
-        '{"name":"' +
-          this.createCloakForm.value["cloakName"] +
-          '", "value": ' +
-          this.createCloakForm.value["cloakValue"] +
-          "}"
-      );
-
-      const postUrlCloak = "http://localhost:3000/cloak";
-      this.http.post(postUrlCloak, jsonCloak).subscribe();
-    } else {
-      this.submitted = true;
-      const jsonUpdateCloak = JSON.parse(
-        '{"name":"' +
-          this.createCloakForm.value["cloakName"] +
-          '", "value": ' +
-          this.createCloakForm.value["cloakValue"] +
-          "}"
-      );
-
-      const updateUrlCloak = "http://localhost:3000/cloak/" + requestType;
-      this.http.put(updateUrlCloak, jsonUpdateCloak).subscribe();
-    }
+    const postUrlCloak = "http://localhost:3000/cloak";
+    this.http.post(postUrlCloak, jsonCloak).subscribe();
   }
 
   onDeleteCloak(cloakId) {
@@ -68,16 +55,19 @@ export class CloakComponent implements OnInit {
     this.http.delete(urlCloak).subscribe();
   }
 
-  onUpdateCloak(cloakName, cloakValue, cloakId) {
-    document.getElementById("submit").classList.remove("btn-success");
-    document.getElementById("submit").classList.add("btn-warning");
-    document.getElementById("submit").innerHTML = "Edit cloak";
-    (document.getElementById("type") as HTMLInputElement).value = cloakId;
-    (document.getElementById(
-      "cloakName"
-    ) as HTMLInputElement).placeholder = cloakName;
-    (document.getElementById(
-      "cloakValue"
-    ) as HTMLInputElement).placeholder = cloakValue;
+  onUpdateCloak(cloakId) {
+    const urlCloakModif = "http://localhost:3000/cloak/" + cloakId;
+    this.http
+      .put(
+        urlCloakModif,
+        JSON.parse(
+          '{"name":"' +
+            this.modificationCloakForm.value["cloakName"] +
+            '", "value": ' +
+            this.modificationCloakForm.value["cloakValue"] +
+            "}"
+        )
+      )
+      .subscribe();
   }
 }
